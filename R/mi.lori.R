43eabb34ae2f32f3eeef1324c74cc21d4fe5e5ee
Genevieve Robin <genevieve.robin@polytechnique.edu>
2019-03-07 05:20:09 +0000

    version 2.1.2

--- R/mi.lori.R
+++ R/mi.lori.R
@@ -1,4 +1,4 @@
-#' Multiple imputation of count data using side information
+#' multiple imputation of count data using the lori model
 #'
 #' @param Y [matrix, data.frame] count table (nxp).
 #' @param cov [matrix, data.frame] design matrix (np*q) in order row1xcol1,row2xcol2,..,rownxcol1,row1xcol2,row2xcol2,...,...,rownxcolp
@@ -9,6 +9,7 @@
 #' @param reff [boolean] whether row effects should be fitted, default value is TRUE
 #' @param ceff [boolean] whether column effects should be fitted, default value is TRUE
 #' @param rank.max [integer] maximum rank of interaction matrix (smaller than min(n-1,p-1))
+#' @param algo type of algorithm to use, either one of "mcgd" (mixed coordinate gradient descent, adapted to large dimensions) or "alt" (alternating minimization, adapted to small dimensions)
 #' @param thresh [positive number] convergence tolerance of algorithm, by default \code{1e-6}.
 #' @param maxit [integer] maximum allowed number of iterations.
 #' @param trace.it [boolean] whether convergence information should be printed
@@ -33,49 +34,96 @@ mi.lori <-   function(Y,
                       cov = NULL,
                       lambda1 = NULL,
                       lambda2 = NULL,
-                      M = 100,
-                      prob=0.1,
+                      M = 25,
+                      prob = 0.1,
                       reff = T,
                       ceff = T,
                       rank.max = 10,
+                      algo = c("alt", "mcgd"),
                       thresh = 1e-5,
                       maxit = 1e3,
-                      trace.it = F){
-
-  ylist <- lapply(1:M, function(k) boot.lori(Y, prob))
-  reslist <- lapply(1:M, function(k){
-    cat('\r', round(100*k/M), "%", sep="")
-    return(lori(ylist[[k]], cov, lambda1, lambda2, reff, ceff, rank.max,
-                thresh, maxit, trace.it))
+                      trace.it = F) {
+  if (round(sqrt(M)) < sqrt(M))
+    M <- round(sqrt(M)) + 1
+  else
+    M <- round(sqrt(M))
+  ylist <- lapply(1:M, function(k)
+    boot.lori(Y))
+  reslist <- lapply(1:M, function(k) {
+    cat('\r', round(100 * k / (2 * M)), "%", sep = "")
+    return(lori(
+      Y=ylist[[k]],
+      cov=cov,
+      lambda1=lambda1,
+      lambda2=lambda2,
+      reff=reff,
+      ceff=ceff,
+      rank.max=rank.max,
+      algo=algo,
+      thresh=thresh,
+      maxit=maxit,
+      trace.it=trace.it
+    ))
   })
-  mi.imputed <- lapply(reslist, function(res) res$imputed)
-  mi.means <- lapply(reslist, function(res) res$means)
-  mi.alpha <- t(sapply(reslist, function(res) res$alpha))
+  d <- dim(Y)
+  n <- d[1]
+  p <- d[2]
+  mi.imputed <- lapply(reslist, function(res)
+    lapply(1:M, function(m)
+      matrix(stats::rpois(
+        n = n * p, lambda = c(res$imputed)
+      ), nrow = n)))
+  mi.imputed <- unlist(mi.imputed, recursive = F)
+  reslist <- lapply(1:length(mi.imputed), function(m) {
+    cat('\r', round(50 + 100 * m / (2 * M ^ 2)), "%", sep = "")
+    return(
+      lori(
+        Y = mi.imputed[[m]],
+        cov = cov,
+        lambda1 = lambda1,
+        lambda2=lambda2,
+        reff=reff,
+        ceff=ceff,
+        rank.max=rank.max,
+        algo=algo,
+        thresh=thresh,
+        maxit=maxit,
+        trace.it=trace.it
+      )
+    )
+  })
+  mi.alpha <- t(sapply(reslist, function(res)
+    res$alpha))
   colnames(mi.alpha) <- rownames(Y)
-  mi.beta <- t(sapply(reslist, function(res) res$beta))
+  mi.beta <- t(sapply(reslist, function(res)
+    res$beta))
   colnames(mi.beta) <- colnames(Y)
-  mi.epsilon <- t(sapply(reslist, function(res) res$epsilon))
+  mi.epsilon <- t(sapply(reslist, function(res)
+    res$epsilon))
   colnames(mi.epsilon) <- colnames(cov)
-  mi.theta <- lapply(reslist, function(res) res$theta)
-  return(list(mi.imputed=mi.imputed, mi.means=mi.means,
-              mi.alpha=mi.alpha, mi.beta=mi.beta,
-              mi.epsilon=mi.epsilon, mi.theta=mi.theta,
-              mi.y = ylist, Y=Y))
+  mi.theta <- lapply(reslist, function(res)
+    res$theta)
+  return(
+    list(
+      mi.imputed = mi.imputed,
+      mi.alpha = mi.alpha,
+      mi.beta = mi.beta,
+      mi.epsilon = mi.epsilon,
+      mi.theta = mi.theta,
+      mi.y = ylist,
+      Y = Y
+    )
+  )
 }
 
-boot.lori <- function(Y, prob=0.1){
+boot.lori <- function(Y) {
   Y <- as.matrix(Y)
   d <- dim(Y)
   n <- d[1]
   p <- d[2]
-  n_na <- round(prob*sum(!is.na(Y)))
-  n_na_sites <- round(n_na/n)
-  n_na_years <- round(n_na/p)
-  idx_sites <- sample(which(rowSums(!is.na(Y))>n_na_sites+1))
-  idx_years <- sample(which(colSums(!is.na(Y))>n_na_years+1))
-  yp <- Y[idx_sites,idx_years]
-  yp[sample(which(!is.na(yp)), n_na)] <- NA
-  Y[idx_sites,idx_years] <- yp
-  Y[which(!is.na(Y))] <- stats::rpois(sum(!is.na(Y)), c(Y[which(!is.na(Y))]))
+  m <- sum(!is.na(Y))
+  new_counts <-
+    stats::rmultinom(1, size = sum(Y, na.rm = T), prob = Y[!is.na(Y)] / m)
+  Y[!is.na(Y)] <- new_counts
   return(Y)
 }
